37b4d310475cffc11959fa2ca08a1e2536b00d6e
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-03-12 16:09:41 +0500

    Component communication using ViewChild

---
+++ angular-recipies/child_parent_component_viewchild/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
